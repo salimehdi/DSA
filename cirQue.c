10d60b8ad3e1af8b10efa44debba482c96a26898
Syed Ali Mehdi <50301227+salimehdi@users.noreply.github.com>
2023-09-26 23:16:08 +0530

    Made cirQue.c Consice

--- cirQue.c
+++ cirQue.c
@@ -9,83 +9,66 @@ void display();
 
 int queue[MAX], front = -1, rear = -1;
 
-int main()
-{
+int main() {
     int ch;
-    printf("\n1.enqueue 2.dequeue 3.display 4.exit \n");
-    while (1)
-    {
+    printf("1. Enqueue 2. Dequeue 3. Display 4. Exit\n");
+    while (1) {
         printf("==> Enter your choice: ");
         scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            enqueue();
-            break;
-        case 2:
-            dequeue();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            exit(0);
-        default:
-            printf("invalid choice\n");
+        switch (ch) {
+            case 1: enqueue(); break;
+            case 2: dequeue(); break;
+            case 3: display(); break;
+            case 4: exit(0);
+            default: printf("Invalid choice\n");
         }
     }
 }
 
-int takeAValue () {
-    int en = 0 ;
+int takeAValue() {
+    int en;
     printf("Enter a number: ");
-    scanf("%d",&en);
-    return en ;
+    scanf("%d", &en);
+    return en;
 }
 
-void enqueue(){
-
-    if ( front - rear == MAX-1 || rear - front == MAX-1 ) {
-        printf("Queue Is Full !\n");
+void enqueue() {
+    if ((front - rear == MAX - 1) || (rear - front == MAX - 1)) {
+        printf("Queue Is Full!\n");
     } else {
         int en = takeAValue();
-        if (front == -1 && rear == -1)
-            front = (front+1)%MAX;
-        rear = (rear+1)%MAX;
-        queue[rear] = en ;
-    } 
-}
-void dequeue(){
-
-    if (front == -1 && rear == -1) {
-        printf("Queue Is Empty !\n");   
-    } else if ( front - rear != 0 ) {
-        printf("%d is removed from queue!\n",queue[front]);
-        front = (front+1)%MAX ;
-    } else if ( front == rear ) {
-        printf("%d is removed. Queue Became Empty !\n",queue[front]);   
-        front = -1 ;
-        rear = -1 ;
+        if (front == -1)
+            front = 0;
+        rear = (rear + 1) % MAX;
+        queue[rear] = en;
     }
-
 }
-void display(){
 
-    if (front == -1 && rear == -1){
-        printf("Empty Queue Hai ?\n");
-    } else { 
-        int i = front ;
+void dequeue() {
+    if (front == -1 && rear == -1) {
+        printf("Queue Is Empty!\n");
+    } else {
+        printf("%d is removed from queue!\n", queue[front]);
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            front = (front + 1) % MAX;
+        }
+    }
+}
 
-        while (1) { // infinite looop !
-            printf("%d[%d] ", queue[i],i);
-            if (i == rear) { 
-                break; // controller
+void display() {
+    if (front == -1 && rear == -1) {
+        printf("Empty Queue!\n");
+    } else {
+        int i = front;
+        printf("Queue: ");
+        while (1) {
+            printf("%d[%d] ", queue[i], i);
+            if (i == rear) {
+                break;
             }
-            if ( i == MAX-1 ){
-                i = 0 ;
-            } else {
-                i++ ;
-            }
+            i = (i + 1) % MAX;
         }
         printf("\n");
     }
